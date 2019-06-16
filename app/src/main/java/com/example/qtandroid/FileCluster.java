6cfe73973aae9090f3899dac2df383823fdd11aa
peppocola <giuseppecolavito999@gmail.com>
2019-06-16 11:24:20 +0200

    removed useless code

--- app/src/main/java/com/example/qtandroid/FileCluster.java
+++ app/src/main/java/com/example/qtandroid/FileCluster.java
@@ -14,11 +14,8 @@ public class FileCluster extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        setTheme(ThemeUtils.defaultTheme());
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_cluster);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
--- app/src/main/java/com/example/qtandroid/NewCluster.java
+++ app/src/main/java/com/example/qtandroid/NewCluster.java
@@ -14,11 +14,8 @@ public class NewCluster extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        setTheme(ThemeUtils.defaultTheme());
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_cluster);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
